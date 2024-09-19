5274298e300d315c00a4e8e6ce2785b096a83f85
Mucino11 <64760426+Mucino11@users.noreply.github.com>
2024-09-19 13:31:32 +0200

    Update Grid.tsx

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,24 +1,3 @@
-// import { useState } from "react";
-// import Student from "./Student";
-// import type { Student as StudentProp } from "./Type";
-
-// type GridProps = {
-//   students: StudentProp[];
-// };
-
-// export default function Grid(props: GridProps) {
-//   // props.students ?? []
-//   const [students, setStudents] = useState(props.students ?? []);
-
-//   return (
-//     <article className="grid">
-//       {students.map((student) => (
-//         <Student key={student.id} name={student.name} id={student.id} />
-//       ))}
-//     </article>
-//   );
-// }
-
 import { useState } from "react";
 import Student from "./Student";
 import AddStudentForm from "./AddStudentForm";
@@ -37,12 +16,12 @@ export default function Grid(props: GridProps) {
 
   return (
     <div>
-      <AddStudentForm onAddStudent={handleAddStudent} />
       <article className="grid">
         {students.map((student) => (
           <Student key={student.id} name={student.name} id={student.id} />
         ))}
       </article>
+      <AddStudentForm onAddStudent={handleAddStudent} />
     </div>
   );
 }
